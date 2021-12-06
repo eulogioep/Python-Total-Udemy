35d677fc5b2bd2fe4b49a28494de3a7f81949e02
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-12-06 20:21:44 +0100

    Fin del tema print

---
+++ Practica_Print/PrimerProyectoPrint.py
@@ -0,0 +1,8 @@
+# Con print podemos mostrar por consola el contenido del paréntesis.
+
+# Strings
+print("Hola Mundo")
+print('He mostrado en consola: "Hola Mundo"')
+
+# Números u operaciones
+print(100); print(10 + 5)
