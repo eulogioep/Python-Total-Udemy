e8fa9d04497176fc647259c27eb929933ec7fcb7
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-12-08 13:37:13 +0100

    Proyecto del Día 1 terminado: Programa un Creador de Nombres

---
+++ CreadorDeNombres/ProyectoCreadorNombres.py
@@ -0,0 +1,9 @@
+# Práctica del Proyecto del Día 1
+# Hay que hacer un programa que pregunte al usuario dos preguntas y muestre en pantalla
+# el nombre generado para una marca de cervezas. Las preguntas tienen que ser originales
+# para que el nombre sea gracioso. Para incrementar el desafío, el nombre tiene que ir
+# entre ""
+
+print("La cerveza se llama \"" + input("¿Cómo se llama tu planeta favorito?: ") + " " +
+      input("¿Cuál es el nombre de tu primera mascota?: ") + "\"")
+
---
+++ CreadorDeNombres/TeoriaInput.py
@@ -0,0 +1,5 @@
+# Con Input podemos hacer que el usuario introduza datos para poder utilizarlo posteriormente
+
+# print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+
