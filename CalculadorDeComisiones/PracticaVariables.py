5847bcf4c78ba844a7d99f249aabf7c05f4ed279
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-12-09 19:14:09 +0100

    Día 2. Variables

--- CalculadorDeComisiones/PracticaVariables.py
+++ CalculadorDeComisiones/PracticaVariables.py
@@ -1,3 +1,12 @@
 # En las variables podemos almacenar datos, los cuales se pueden modificar, recuperar o eliminar
 # según nos interese.
 
+# Creo una variable llamada nombre y le asigno el valor de mi nombre
+nombre = "Eulogio"
+
+# Muestro en pantalla el valor de mi variable
+print(nombre)
+
+# También podemos pedir a un usuario que introduzca un dato
+edad = input("Dime tu edad: ")
+print("Tu edad es: " + edad)
