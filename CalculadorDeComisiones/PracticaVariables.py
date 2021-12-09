447158f10da46e28e4c8dd6d8c54339dc0182ef6
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-12-09 16:04:31 +0100

    Día 2. Variables

---
+++ CalculadorDeComisiones/PracticaVariables.py
@@ -0,0 +1,3 @@
+# En las variables podemos almacenar datos, los cuales se pueden modificar, recuperar o eliminar
+# según nos interese.
+
---
+++ CalculadorDeComisiones/TiposDeDatos.py
@@ -0,0 +1,21 @@
+# En Python hay una variedad de tipos de datos:
+#
+# Cadena de caractéres: string (str)      "hola", "%$&", " ", "123"
+
+# Números enteros:      integer (int)     150, 1, 555, -15, 0
+
+# Números decimales:    floating (float)  1.25, 25.0, 500.50, -95.1
+
+# Listas donde pueden contener varios tipos de datos. Tienen un índice que empieza
+# por 0: (lst) ["sal", 1, 4.5, "marte"]
+
+# Diccionarios donde hay pares de palabras agrupadas, clave : valor. No hay índice
+# de orden: (dic) {'color':'rojo', 'arte':'cine'}
+
+# Tuples. El orden de los datos que contiene es inmutable. No se puede modificar su posición:
+# (tup) ('lun','mar','mie','jue','vier')
+
+# Sets. Es un conjunto de elementos ordenados ÚNICOS. A diferencia de las Listas o los Tuples,
+# en los Sets no pueden contener elementos repetidos: (set) {'a','b','c','d','e'}
+
+# Booleanos. Valores lógicos de True o False: (bool) True, False
